27fd1cf763abe1b348559f9ba1c5ec6a1b84caaf
David <david@davidzech.com>
2020-01-14 22:20:50 -0800

    Remove failNow

--- main_test.go
+++ main_test.go
@@ -13,7 +13,6 @@ import (
 )
 
 func TestNotes(t *testing.T) { // Test Suite is called TestNotes
-	t.FailNow()
 	router := createRouter()
 	t.Run("create", func(t *testing.T) { // one test case
 		recorder := httptest.NewRecorder()
